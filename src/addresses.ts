bbf99bed7c8a1d465d5d180e6abdf44cc16624ff
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2021-02-26 17:04:34 +0900

    update by lint

--- src/addresses.ts
+++ src/addresses.ts
@@ -1,4 +1,6 @@
 import { FunctionAddresses } from '@devprotocol/khaos-core'
 
 export const addresses: FunctionAddresses = async ({ network }) =>
-	network === 'mainnet' ? '0x886f06F5118536589e89A719d3D9E61B330E95B6' : '0x1CF5A65D5594C507D797c855D71cF5524B15a639'
+	network === 'mainnet'
+		? '0x886f06F5118536589e89A719d3D9E61B330E95B6'
+		: '0x1CF5A65D5594C507D797c855D71cF5524B15a639'
