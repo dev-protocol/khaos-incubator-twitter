1e343c635167dbe1ea2060eb49a817e49157a3c3
aggre <hiroyuki.aggre@gmail.com>
2021-03-01 11:43:06 +0900

    apply formatter

--- src/oraclize.ts
+++ src/oraclize.ts
@@ -2,9 +2,7 @@ import urljoin from 'url-join'
 import { FunctionOraclizer } from '@devprotocol/khaos-core'
 import { execute } from './programmable-proxy'
 
-export const oraclize: FunctionOraclizer = async ({
-	query,
-}) => {
+export const oraclize: FunctionOraclizer = async ({ query }) => {
 	const twitterId: string = query.allData['_twitterId']
 	const repository: string = query.allData['_githubRepository']
 	const twitterUrl = urljoin('https://api.twitter.com/2/tweets', twitterId)
