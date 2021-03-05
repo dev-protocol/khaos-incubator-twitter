41956bae8f373f75fd626e2b20437bcd747c1dae
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2021-03-05 10:11:41 +0900

    update address

--- src/addresses.test.ts
+++ src/addresses.test.ts
@@ -3,10 +3,10 @@ import { addresses } from './addresses'
 
 test('Returns mainnet address', async (t) => {
 	const res = await addresses({ network: 'mainnet' })
-	t.is(res, '0x02d107567020bCE1Eb524D80C44f899Fa9993E5f')
+	t.is(res, '0x7f1b8c30832ca3ABC6326A58903A3a47ade00019')
 })
 
 test('Returns ropsten address', async (t) => {
 	const res = await addresses({ network: 'ropsten' })
-	t.is(res, '0x554c3f103894901f4ea0c4c955EB0E6fB347b5A6')
+	t.is(res, '0x59D4b114866920eD57FfAdF14A9E84a11e41217B')
 })
