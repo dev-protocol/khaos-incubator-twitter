18a075d3bca50eba431d3cfadb42f02fa81e89f6
aggre <hiroyuki.aggre@gmail.com>
2021-06-06 01:40:43 +0900

    bug fix

--- src/oraclize.ts
+++ src/oraclize.ts
@@ -9,15 +9,15 @@ export const oraclize: FunctionOraclizer = async ({ query }) => {
 	const repository: string = query.allData['_githubRepository']
 	const [isStatusGreen, urls] = await getTextUrls(twitterId)
 	const stakesSocialUrls = urls.filter(
-		(url) => url.indexOf('https://stakes.social') !== -1
+		(url?: string) => url?.indexOf('https://stakes.social') !== -1
 	)
 	const resStatus = stakesSocialUrls.length > 0 ? 0 : 1
 	const resStatusMessage =
 		resStatus === 0
 			? ''
 			: isStatusGreen
-				? 'stakes social url is not included'
-				: 'twitter api error'
+			? 'stakes social url is not included'
+			: 'twitter api error'
 	return {
 		message: repository,
 		status: resStatus,
