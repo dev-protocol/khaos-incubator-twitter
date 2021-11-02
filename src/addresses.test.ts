15659002f7bed56a3135856cbca151a307ba3660
aggre <hiroyuki.aggre@gmail.com>
2021-11-03 00:08:12 +0900

    returns undefined

--- src/addresses.test.ts
+++ src/addresses.test.ts
@@ -10,3 +10,13 @@ test('Returns ropsten address', async (t) => {
 	const res = await addresses({ network: 'ropsten' })
 	t.is(res, '0xca98de1774F13090014660fb80367Fde970C4A72')
 })
+
+test('Returns undefined when arbitrum-one', async (t) => {
+	const res = await addresses({ network: 'arbitrum-one' })
+	t.is(res, undefined)
+})
+
+test('Returns undefined when arbitrum-rinkeby', async (t) => {
+	const res = await addresses({ network: 'arbitrum-rinkeby' })
+	t.is(res, undefined)
+})
--- src/addresses.ts
+++ src/addresses.ts
@@ -3,4 +3,6 @@ import { FunctionAddresses } from '@devprotocol/khaos-core'
 export const addresses: FunctionAddresses = async ({ network }) =>
 	network === 'mainnet'
 		? '0x7f1b8c30832ca3ABC6326A58903A3a47ade00019'
-		: '0xca98de1774F13090014660fb80367Fde970C4A72'
+		: network === 'ropsten'
+		? '0xca98de1774F13090014660fb80367Fde970C4A72'
+		: undefined
